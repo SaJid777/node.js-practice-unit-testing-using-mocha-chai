9816683ee452f0ce7972dcfe71aa9aac9b5ab96b
Md. Sajid Salim <sajidsalim46@gmail.com>
2021-12-18 23:46:03 +0600

    playing with mocha chai

---
+++ test/practiceTest.spec.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('user get api', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('single user api', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('update user api', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('user daily checkin api', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('create api', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
